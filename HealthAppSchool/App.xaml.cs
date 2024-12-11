bf778b97dd1c7a6bf1e1a3ccbc8bb052dbc12c5c
game1d <jordypluijmen@live.nl>
2024-12-11 16:17:13 +0100

    Add project files.

---
+++ HealthAppSchool/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthAppSchool
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
