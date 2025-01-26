41928a70f36c6238b8fa932cb25256ff0fa47758
maria <maria@LAPTOP-TT5U0GN0>
2025-01-26 18:52:19 +0100

    gewerkt

--- HealthAppSchool/Pages/MedicijnPage.xaml.cs
+++ HealthAppSchool/Pages/MedicijnPage.xaml.cs
@@ -1,14 +1,27 @@
+using HealthAppSchool.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+
 namespace HealthAppSchool.Pages;
 
 public partial class MedicijnPage : ContentPage
 {
-	public MedicijnPage()
+    public List<Medicijn> medicijnen { get; set; } = new List<Medicijn>();
+    
+    public MedicijnPage()
 	{
 		InitializeComponent();
 	}
 
-    private void LvMedicijn_ItemTapped(object sender, ItemTappedEventArgs e)
+    private async void LvMedicijn_ItemTapped(object sender, ItemTappedEventArgs e)
     {
+        if (e.Item == null)
+            return;
 
+        var selectedMedicijn = e.Item as Medicijn;
+        if (selectedMedicijn != null)
+        {
+           // await Navigation.PushAsync(new MedicijnBestellerPage(Database, selectedMedicijn));
+        }
+        ((ListView)sender).SelectedItem = null;
     }
 }
